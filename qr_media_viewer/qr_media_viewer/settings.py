7ca63df42926b737ad368293206da4f8e187f51b
Moremi777 <olebogengmoremi84@gmail.com>
2025-01-13 23:33:55 +0200

    changes

--- qr_media_viewer/qr_media_viewer/settings.py
+++ qr_media_viewer/qr_media_viewer/settings.py
@@ -123,11 +123,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
+STATIC_URL = '/static/'
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
-    BASE_DIR / "static",  # Adjust if your static directory is elsewhere
+    os.path.join(BASE_DIR, 'static'),
 ]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
