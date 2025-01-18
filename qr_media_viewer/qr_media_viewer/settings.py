ca1aaa3626516bb9504a484bf0e0d21bcb0128cf
Moremi777 <olebogengmoremi84@gmail.com>
2025-01-18 17:11:51 +0200

    last deploy

--- qr_media_viewer/qr_media_viewer/settings.py
+++ qr_media_viewer/qr_media_viewer/settings.py
@@ -25,21 +25,24 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-s%2he=yt^2hk_f1-gsac9krl3il(e(c5l+#mv29r&0*e(zg8+s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
     'localhost',
-    'qr-media.onrender.com',  # Your live domain
+    'qr-media.onrender.com',
+    'qrcode-app.onrender.com',  # Your live domain
 ]
 
 CSRF_TRUSTED_ORIGINS = [
-    'https://qr-media.onrender.com',  # Add your live domain
+    'https://qr-media.onrender.com',
+    'https://qrcode-app.onrender.com',  # Add your live domain
 ]
 
 
 CORS_ALLOWED_ORIGINS = [
     'https://qr-media.onrender.com',
+    'https://qrcode-app.onrender.com',
 ]
 
 # Application definition
