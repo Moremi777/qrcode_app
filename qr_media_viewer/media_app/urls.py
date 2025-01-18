ec4f16bb56a1983f5d244b851dfe2814371dd0aa
Moremi777 <olebogengmoremi84@gmail.com>
2025-01-18 16:40:25 +0200

    final commit

--- qr_media_viewer/media_app/urls.py
+++ qr_media_viewer/media_app/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('upload/', views.upload_media, name='upload'),
     path('media/<int:media_id>/', views.view_media, name='view_media'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
