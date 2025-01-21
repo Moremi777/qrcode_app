dab5c805e34bdad59086d5236b1d50c7fe2d6efe
Moremi777 <olebogengmoremi84@gmail.com>
2025-01-21 12:22:39 +0200

    ...

--- qr_media_viewer/qr_media_viewer/settings.py
+++ qr_media_viewer/qr_media_viewer/settings.py
@@ -89,7 +89,7 @@ WSGI_APPLICATION = 'qr_media_viewer.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-'''
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',  # Database backend (SQLite in this case)
@@ -110,7 +110,7 @@ DATABASES = {
         'PORT': os.getenv('DATABASE_PORT', 5432),
     }
 }
-
+'''
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
